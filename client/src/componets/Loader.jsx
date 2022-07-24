9d5d2e0e968fd8569fab903604ea3b3ea373a69d
dewanshiPaul <dewanshipaul84@gmail.com>
2022-07-24 16:48:04 +0530

    welcome page layout

--- client/src/componets/Loader.jsx
+++ client/src/componets/Loader.jsx
@@ -1,5 +1,7 @@
 export function Loader() {
     return (
-        <div>Loader</div>
+        <div className="flex justify-center items-center py-3">
+            <svg className="animate-spin h-20 w-20 rounded-full border-b-2 border-red-700"/>
+        </div>
     )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,48 @@
 module.exports = {
-  content: [
+  mode: 'jit',
+  purge: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
+  darkMode: 'class',
   theme: {
+    extend: {
+      screens: {
+        mf: "800px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          }
+        },
+        "slide-out": {
+          "0%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          }
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.2s ease-in-out",
+        "slide-out": "slide-in 0.2s ease-in-out",
+      }
+    },
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
